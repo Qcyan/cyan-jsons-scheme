327b465d6039fd4bfd769ba260387e03a4578e1b
cyan <1165518359@qq.com>
2024-09-09 16:17:40 +0800

    first

--- vite.config.ts
+++ vite.config.ts
@@ -6,13 +6,24 @@ import vueJsx from '@vitejs/plugin-vue-jsx'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-    vueJsx(),
-  ],
+  plugins: [vue(), vueJsx()],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  },
+  server: {
+    port: 9999,
+    hmr: {
+      // overlay: false
+    },
+    host: '0.0.0.0',
+    proxy: {
+      '/api': {
+        target: 'http://192.168.3.253:9770/',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, '')
+      }
     }
   }
 })
