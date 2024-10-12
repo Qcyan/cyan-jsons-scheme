f5bd70114d723fd6b8162e086724631fbac82624
cyan <1165518359@qq.com>
2024-10-12 18:01:45 +0800

    demo

--- README.md
+++ README.md
@@ -1,39 +1,67 @@
-# vue-project
-
-This template should help get you started developing with Vue 3 in Vite.
-
-## Recommended IDE Setup
-
-[VSCode](https://code.visualstudio.com/) + [Volar](https://marketplace.visualstudio.com/items?itemName=Vue.volar) (and disable Vetur).
-
-## Type Support for `.vue` Imports in TS
-
-TypeScript cannot handle type information for `.vue` imports by default, so we replace the `tsc` CLI with `vue-tsc` for type checking. In editors, we need [Volar](https://marketplace.visualstudio.com/items?itemName=Vue.volar) to make the TypeScript language service aware of `.vue` types.
-
-## Customize configuration
-
-See [Vite Configuration Reference](https://vitejs.dev/config/).
-
-## Project Setup
-
-```sh
-pnpm install
+```ruby
+<JsonSchemaForm
+  schema={schema}
+  value={value}
+  onChange={handleChange}
+  locale={locale}
+  contextRef={someRef}
+  uiSchema={uiSchema}
+/>
 ```
 
-### Compile and Hot-Reload for Development
+#### schema
 
-```sh
-pnpm dev
+json schema 对象，用来定义数据，同时也是我们定义表单的依据
+
+#### value
+
+表单的数据结果，你可以从外部改变这个 value，在表单被编辑的时候，会通过onChange透出 value
+
+需要注意的是，因为 vue 使用的是可变数据，如果每次数据变化我们都去改变value的对象地址，那么会导致整个表单都需要重新渲染，这会导致性能降低。 从实践中来看，我们传入的对象，在内部修改其 field 的值基本不会有什么副作用，所以我们会使用这种方式来进行实现。也就是说，如果value是一个对象， 那么从JsonSchemaForm内部修改的值，并不会改变value对象本身。我们仍然会触发onChange，因为可能在表单变化之后，使用者需要进行一些操作。
+
+#### onChange
+
+在表单值有任何变化的时候会触发该回调方法，并把新的值进行返回
+
+#### locale
+
+语言，使用ajv-i18n指定错误信息使用的语言
+
+#### contextRef
+
+你需要传入一个 vue3 的Ref对象，我们会在这个对象上挂载doValidate方法，你可以通过
+
+```
+const yourRef = ref({})
+
+onMounted(() => {
+yourRef.value.doValidate()
+})
+
+<JsonSchemaForm contextRef={yourRef} />
 ```
 
-### Type-Check, Compile and Minify for Production
+这样来主动让表单进行校验。
 
-```sh
-pnpm build
+#### uiSchema
+
+对表单的展现进行一些定制，其类型如下：
+
 ```
-
-### Lint with [ESLint](https://eslint.org/)
-
-```sh
-pnpm lint
+export interface VueJsonSchemaConfig {
+  title?: string
+  descrription?: string
+  component?: string
+  additionProps?: {
+    [key: string]: any
+  }
+  withFormItem?: boolean
+  widget?: 'checkbox' | 'textarea' | 'select' | 'radio' | 'range' | string
+  items?: UISchema | UISchema[]
+}
+export interface UISchema extends VueJsonSchemaConfig {
+  properties?: {
+    [property: string]: UISchema
+  }
+}
 ```
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,5 +1,160 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { defineComponent, ref, Ref, reactive, watchEffect } from 'vue'
+import { createUseStyles } from 'vue-jss'
+import MonacoEditor from '@/components/MonacoEditor'
+
+// schema
+import demos from '@/demos'
+// TODO: 在lib中export
+type Schema = any
+type UISchema = any
+
+function toJson(data: any) {
+  return JSON.stringify(data, null, 2)
+}
+
+// 样式
+const useStyles = createUseStyles({
+  container: {
+    display: 'flex',
+    flexDirection: 'column',
+    height: '100%',
+    width: '1200px',
+    margin: '0 auto'
+  },
+  menu: {
+    marginBottom: 20
+  },
+  code: {
+    width: 700,
+    flexShrink: 0
+  },
+  codePanel: {
+    minHeight: 400,
+    marginBottom: 20
+  },
+  uiAndValue: {
+    display: 'flex',
+    justifyContent: 'space-between',
+    '& > *': {
+      width: '46%'
+    }
+  },
+  content: {
+    display: 'flex'
+  },
+  form: {
+    padding: '0 20px',
+    flexGrow: 1
+  },
+  menuButton: {
+    appearance: 'none',
+    borderWidth: 0,
+    backgroundColor: 'transparent',
+    cursor: 'pointer',
+    display: 'inline-block',
+    padding: 15,
+    borderRadius: 5,
+    '&:hover': {
+      background: '#efefef'
+    }
+  },
+  menuSelected: {
+    background: '#337ab7',
+    color: '#fff',
+    '&:hover': {
+      background: '#337ab7'
+    }
+  }
+})
+// 变量
+const classesRef = useStyles()
+const classes = classesRef.value
+const selected: Ref<number> = ref(0)
+const methodRef: Ref<any> = ref()
+const demo: {
+  schema: Schema | null
+  data: any
+  uiSchema: UISchema | null
+  schemaCode: string
+  dataCode: string
+  uiSchemaCode: string
+} = reactive({
+  schema: null,
+  data: {},
+  uiSchema: {},
+  schemaCode: '',
+  dataCode: '',
+  uiSchemaCode: ''
+})
+
+watchEffect(() => {
+  const index = selected.value
+  const d = demos[index]
+  demo.schema = d.schema
+  demo.data = d.default
+  demo.uiSchema = d.uiSchema
+  demo.schemaCode = toJson(d.schema)
+  demo.dataCode = toJson(d.default)
+  demo.uiSchemaCode = toJson(d.uiSchema)
+})
+const handleChange = (v: any) => {
+  demo.data = v
+  demo.dataCode = toJson(v)
+}
+
+function handleCodeChange(filed: 'schema' | 'data' | 'uiSchema', value: string) {
+  try {
+    const json = JSON.parse(value)
+    demo[filed] = json
+    ;(demo as any)[`${filed}Code`] = value
+  } catch (err) {
+    // some thing
+  }
+}
+
+const handleSchemaChange = (v: string) => handleCodeChange('schema', v)
+const handleDataChange = (v: string) => handleCodeChange('data', v)
+const handleUISchemaChange = (v: string) => handleCodeChange('uiSchema', v)
+</script>
 
 <template>
   <main>132</main>
+  <div :class="classes.container">
+    <div :class="classes.menu">
+      <h1>Vue3 JsonSchema Form</h1>
+      <div v-for="(demo, index) in demos" :key="index">
+        <button
+          :class="[classes.menuButton, selected === index ? classes.menuSelected : '']"
+          @click="selected = index"
+        >
+          {{ demo.name }}
+        </button>
+      </div>
+    </div>
+    <div :class="classes.content">
+      <div :class="classes.code">
+        <MonacoEditor
+          :code="demo.schemaCode"
+          :class="classes.codePanel"
+          @Change="handleSchemaChange"
+          title="Schema"
+        />
+        <div :class="classes.uiAndValue">
+          <MonacoEditor
+            :code="demo.uiSchemaCode"
+            :class="classes.codePanel"
+            @Change="handleUISchemaChange"
+            title="UISchema"
+          />
+          <MonacoEditor
+            :code="demo.dataCode"
+            :class="classes.codePanel"
+            @Change="handleDataChange"
+            title="Value"
+          />
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
